d7f436ab27a1c9dd03e9df04ea8857c7cd1e7387
evanboyle <evan@pulumi.com>
2023-05-14 19:29:50 -0700

    update readme

--- README.md
+++ README.md
@@ -42,3 +42,10 @@ export const a = new AutoDeployer("auto-deployer-a", {
 `AutoDeployer` does a few things:
 1. For each stack create an API Gateway that knows how to trigger an update for the given stack via a call to the Pulumi Deployments REST API.
 2. For each dependent downstream stack, register a Pulumi stack webhook such that when an update finished for stack `A`, a webhook is fired to the update API Gateway function for downstream stack `D`.
+
+To run this example:
+
+1. Initialize and create the six stacks in `./dependency-example` in your org of choice. These are the stacks that have dependencies that will be exercised via `AutoDeploy`.
+1. Configure deployment settings for all six stacks (these are trivial programs that do not require creds).
+2. Create the `pulumi-auto-deploy` example stack and run an update at the root of the directory (requires AWS credentials and exported `PULUMI_ACCESS_TOKEN` environment variables).
+3. Trigger an update on `dependency-example/a` and watch all downstream stacks get updated via a deployment
--- index.ts
+++ index.ts
@@ -22,49 +22,53 @@ const pulumiAccessToken = pulumi.secret(process.env.PULUMI_ACCESS_TOKEN || "");
  * all downstream nodes will be automatically updated via a webhook triggering Pulumi Deployments.
  */
 
+
+const org = pulumi.getOrganization();
+const project = "dependency-example"
+
 export const f = new AutoDeployer("auto-deployer-f", {
-    org: "pulumi",
-    project: "dependency-example",
+    org,
+    project,
     stack: "f",
     downstream: [],
     pulumiAccessToken,
 });
 
 export const e = new AutoDeployer("auto-deployer-e", {
-    org: "pulumi",
-    project: "dependency-example",
+    org,
+    project,
     stack: "e",
     downstream: [],
     pulumiAccessToken,
 });
 
 export const d = new AutoDeployer("auto-deployer-d", {
-    org: "pulumi",
-    project: "dependency-example",
+    org,
+    project,
     stack: "d",
     downstream: [],
     pulumiAccessToken,
 });
 
 export const c = new AutoDeployer("auto-deployer-c", {
-    org: "pulumi",
-    project: "dependency-example",
+    org,
+    project,
     stack: "c",
     downstream: [],
     pulumiAccessToken,
 });
 
 export const b = new AutoDeployer("auto-deployer-b", {
-    org: "pulumi",
-    project: "dependency-example",
+    org,
+    project,
     stack: "b",
     downstream: [d, e, f],
     pulumiAccessToken,
 });
 
 export const a = new AutoDeployer("auto-deployer-a", {
-    org: "pulumi",
-    project: "dependency-example",
+    org,
+    project,
     stack: "a",
     downstream: [b, c],
     pulumiAccessToken,
