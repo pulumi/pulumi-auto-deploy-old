27341834021ecb832db1f1939c054da1a6083c1b
Komal <komal@pulumi.com>
2023-06-02 13:12:38 -0700

    Fix URL

--- service-provider-example/parent/index.ts
+++ service-provider-example/parent/index.ts
@@ -1,12 +1,26 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as service from "@pulumi/pulumiservice";
 
+const settings = new service.DeploymentSettings("settings", {
+    organization: "deploy-testing",
+    project: pulumi.getProject(),
+    stack: pulumi.getStack(),
+    sourceContext: {
+        git: {
+            repoUrl: "https://github.com/pulumi/pulumi-auto-deploy.git",
+            branch: "komal/service-provider",
+            repoDir: "service-provider-example/parent",
+        }
+    }
+});
+
 const webhook = new service.Webhook("webhook", {
     organizationName: "deploy-testing",
     projectName: pulumi.getProject(),
     stackName: pulumi.getStack(),
     format: service.WebhookFormat.PulumiDeployments,
-    payloadUrl: "https://api.komal.pulumi-dev.io/api/preview/deploy-testing/child/dev/deployments",
+    payloadUrl: "child/dev",
     active: true,
-    displayName: "deploy-child-stack"
+    displayName: "deploy-child-stack",
+    filters: [service.WebhookFilters.UpdateSucceeded]
 });
